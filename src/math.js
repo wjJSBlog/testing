8ffde359f3f34beb5ebd413b16edbdb7cd249adf
wjJSBlog <974742841@qq.com>
2018-08-24 12:52:56 +0800

    单元测试以及持续集成

--- src/math.js
+++ src/math.js
@@ -2,8 +2,7 @@ module.exports = {
     add: function (a, b) {
         return a + b;
     },
-
     mul: function (a, b) {
         return a - b;
-    },
+    }
 }
--- test/mocha.js
+++ test/mocha.js
@@ -1,22 +1,3 @@
 const {
   assert
 } = require("chai");
-
-const {
-  add,
-  mul
-} = require("../src/math");
-
-describe('#math', function () {
-  describe('#add()', function () {
-    it('should return 5 when 2+3', function () {
-      assert.equal(add(2 , 3), 5);
-    });
-  });
-
-  describe('#mul()', function () {
-    it('should return 5 when 8-3', function () {
-      assert.equal(mul(8, 3), 5);
-    });
-  });
-});
